2fc5e4f7db4b2c5a2ea1299995bcbf1e8777a6fb
Paul Gould <pagould@gmail.com>
2023-10-07 12:20:33 +0100

    p31

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
 import * as React from "react";
 
-const welcome = {
-  title: "Reactt",
-  greeting: "Hey",
-};
-
-const myArray = [10, 20, 30, 40, 50];
-
-function getTitle() {
-  return welcome.title;
-}
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Hello World!!</h1>
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        ))}
+      </ul>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
