e73e99c129e0b73b0cfa4c9c73e40d588103f09d
Paul Gould <pagould@gmail.com>
2023-10-07 13:39:16 +0100

    react component instantiatin

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,56 @@ import * as React from "react";
 const list = [
   {
     title: "React",
-    url: "https://reactjs.org",
+    url: "https://reactjs.org/",
     author: "Jordan Walke",
     num_comments: 3,
     points: 4,
     objectID: 0,
   },
   {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   },
 ];
 
 function App() {
   return (
     <div>
-      <h1>Hello World!!</h1>
-      <ul>
-        {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        ))}
-      </ul>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List />
+    </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
   );
 }
 
+function List() {
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
 export default App;
