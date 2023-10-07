726785f5a43262f41fcf534304567702b922fb71
Paul Gould <pagould@gmail.com>
2023-10-07 11:44:26 +0100

    p25

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import * as React from "react";
 
-const title = "Hello World!!";
+const welcome = {
+  title: "Reactt",
+  greeting: "Hey",
+};
+
+const myArray = [10, 20, 30, 40, 50];
+
+function getTitle() {
+  return welcome.title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>
+        {getTitle()} {welcome.greeting}
+      </h1>
+      <ul>
+        {myArray.map((number) => (
+          <li>{number * 10}</li>
+        ))}
+      </ul>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
